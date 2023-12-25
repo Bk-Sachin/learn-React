984cf4769ad5c9dbfd16ed00cc299d769f3452d4
Sachin b.k. <sachinbk.official@gmail.com>
2023-12-25 17:28:15 +0545

    second

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,20 @@
-import React from 'react'
+import {BrowserRouter, Routes, Route } from 'react-router-dom';
+import Home from './pages/Home';
+import About from './pages/About';
+import SignIn from './pages/SignIn';
+import SignUp from './pages/Signup';
+import Profile from './pages/Profile';
 
 export default function App() {
-  return (
-    <div className='text-red-500'>App</div>
-  )
+  return <BrowserRouter>
+  <Routes>
+    <Route path="/" element={<Home />} />
+    <Route path="/" element={<About />} />
+    <Route path="/" element={<SignIn />} />
+    <Route path="/" element={<SignUp />} />
+    <Route path="/" element={<Profile />} />
+  </Routes>
+  
+  </BrowserRouter>
+  
 }
