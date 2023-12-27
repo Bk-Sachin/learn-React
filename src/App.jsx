109ba01887fcb4a5c2e1743443e9e4fd33f22b85
Sachin b.k. <sachinbk.official@gmail.com>
2023-12-27 21:09:31 +0545

    minor fixes

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,20 @@
-import {BrowserRouter, Routes, Route } from 'react-router-dom';
-import Home from './pages/Home';
-import About from './pages/About';
-import SignIn from './pages/SignIn';
-import SignUp from './pages/Signup';
-import Profile from './pages/Profile';
+import About from "./pages/About";
+import Home from "./pages/Home";
+import Profile from "./pages/Profile";
+import SignIn from "./pages/SignIn";
+import SignUp from "./pages/SignUp";
+import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 export default function App() {
   return <BrowserRouter>
   <Routes>
-    <Route path="/" element={<Home />} />
-    <Route path="/" element={<About />} />
-    <Route path="/" element={<SignIn />} />
-    <Route path="/" element={<SignUp />} />
-    <Route path="/" element={<Profile />} />
+    <Route path="/" element={<Home/>}></Route>
+    <Route path="/sign-in" element={<SignIn/>}></Route>
+    <Route path="/sign-up" element={<SignUp/>}></Route>
+    <Route path="/profile" element={<Profile/>}></Route>
+    <Route path="/About" element={<About/>}></Route>
+
   </Routes>
-  
   </BrowserRouter>
-  
 }
